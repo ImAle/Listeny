1d4a2b7cd75f8fb05444be6fe40162309e3271c1
Sergio <carreramartinezyt.94@gmail.com>
2023-01-12 18:58:55 +0100

    - Mappeo los campos de la tabla "Artistas" del modelo de datos en la clase "Artistas" en Java.

--- src/main/java/com/listeny/listeny/models/Artistas.java
+++ src/main/java/com/listeny/listeny/models/Artistas.java
@@ -1,10 +1,24 @@
 package com.listeny.listeny.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Artistas")
 public class Artistas {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column (name = "nombre", length = 45)
     private String nombre;
+
+    @Column (name = "idUsuario")
     private int idUsuario;
-
-
 }
